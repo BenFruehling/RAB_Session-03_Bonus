161fed0b92f0d0d617f63503dbd70cac80ae6851
BenFruehling <ben.fruehling@gmail.com>
2022-12-10 14:36:33 -0500

    Add project files.

---
+++ CmdEPPlus.cs
@@ -0,0 +1,34 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using OfficeOpenXml;
+
+#endregion
+
+namespace RAB_Session_03_Bonus
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CmdEPPlus : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            // Access current selection
+
+            return Result.Succeeded;
+        }
+    }
+}
